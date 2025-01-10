41c41219fe29e647b02d1f052c5ca3d70a808daf
Abishekabi34 <abishekashok2021@gmail.com>
2025-01-10 15:40:31 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
-  base: '/Todo-list/',
+  base: '/Todo-list/', // Replace 'repo-name' with your GitHub repository name
   build: {
     outDir: 'dist',
   },
+  plugins:[react()]
 });
